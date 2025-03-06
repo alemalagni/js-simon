ad50e0f138c35e6f85f85af224adbd98aa33b9ea
alemalagni <alemalagni@gmail.com>
2025-03-06 18:24:42 +0100

    feat: add JS time script

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 // Dichiarazione variabili
 const clock = setInterval( scadenza, 1000 );
 let sec = 30;
+const time = document.getElementById("time");
 
 // Funzioni
 function scadenza(){ 
     sec -= 1;
-    console.log(sec)
+    time.innerHTML = sec;
 
     if ( sec === 0 ){
         clearInterval( clock );
